6088ba6d1346e659781f2e628f4942abccf4fd64
Leonard Bruns <leonardb@kth.se>
2023-05-30 10:15:38 +0200

    Save vanishing point associations.

--- runners/pointline_association.py
+++ runners/pointline_association.py
@@ -117,7 +117,10 @@ def pointline_association(cfg, input_folder, output_folder, colmap_folder):
     newtracks = bpt3d.get_all_lines()
     limapio.save_folder_linetracks_with_info(output_folder, newtracks, config=cfg_info, imagecols=imagecols, all_2d_segs=all_2d_segs)
 
+    # save point and vanishing point associations
     np.savez(os.path.join(output_folder, "bpt3d_pl.npz"), bpt3d_pl_np=bpt3d.as_dict())
+    if cfg["global_pl_association"]["use_vp"]:
+        np.savez(os.path.join(output_folder, "bpt3d_vp.npz"), bpt3d_vp_np=bpt3d_vp.as_dict())
 
     # visualize
     if cfg["visualize"]:
